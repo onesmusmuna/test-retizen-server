1b0390f7071529f6adb50d648c7e424faeda99bc
onesmusmuna <onesmusjones@gmail.com>
2024-05-18 11:07:06 +0300

    🔐

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import { config } from "dotenv";
-import app from "./app";
+import { config } from 'dotenv';
+import app from './app';
 config();
 
 (() => {
-  const { DATABASE_URL, PORT, JWT_SECRET } = process.env;
+  const { DATABASE_URL, PORT, JWT_SECRET, ADMIN_EMAIL } = process.env;
 
-  if (!DATABASE_URL || !PORT || !JWT_SECRET) {
-    return console.log("Define ENVs");
+  if (!DATABASE_URL || !PORT || !JWT_SECRET || !ADMIN_EMAIL) {
+    return console.log('Define ENVs');
   }
 
-  app.listen(PORT, () =>
-    console.log(`\nRetizen-server running on port: ${PORT} 👌`)
-  );
+  app.listen(PORT, () => console.log(`\nRetizen-server running on port: ${PORT} 👌`));
 })();
